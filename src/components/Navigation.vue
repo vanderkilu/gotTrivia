ad037915a9da890cc727c23a47d047130755f0c1
vanderkilu <vanderkiliku@gmail.com>
2019-02-12 18:49:03 +0000

    adding and configuring vuerouter

---
+++ src/components/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav class="nav">
+        <router-link :to="{}">Characters</router-link>
+        <router-link :to="{}">Houses</router-link>
+        <router-link :to="{}">Episodes</router-link>
+        <router-link :to="{}">Trivia</router-link>
+    </nav>
+</template>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
@@ -7,5 +8,6 @@ Vue.config.productionTip = false
 export const eventBus = new Vue()
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
