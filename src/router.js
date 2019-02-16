b0e3f9eff32edd57d9a119bb61820699d0ac026e
vanderkilu <vanderkiliku@gmail.com>
2019-02-16 13:47:46 +0000

    changed home route

--- src/router.js
+++ src/router.js
@@ -15,7 +15,7 @@ export default new Router({
     base: process.env.BASE_URL,
     routes: [
         {
-            path: '/',
+            path: '',
             name: 'home',
             component: Home
         }, 
